f41c79ed5554f2d4da0af9f085ccdb37eb78c019
Lenilson Soares <lenilsonp40@gmail.com>
2023-10-16 23:17:19 -0300

    controller

--- Servidor Http/ServidorHttp.cs
+++ Servidor Http/ServidorHttp.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 class ServidorHttp
 {
@@ -8,11 +10,107 @@ class ServidorHttp
 
     public ServidorHttp(int porta = 8080)
     {
-        this.Porta = porta;
+        Porta = porta;
         try
         {
-            this.Controlador = new TcpListener()
+            this.Controlador = new TcpListener(IPAddress.Parse("127.0.0.1"), Porta);
+            this.Controlador.Start();
+            Console.WriteLine($"Servidor HTTP está rodando na porta {Porta}.");
+            Console.WriteLine($"Para acessar, digite no navegador: hhtp://localhost:{Porta}.");
+            Task servidorHttpTask = Task.Run(() => AguardarRequests());
+            servidorHttpTask.GetAwaiter().GetResult();
         }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro ao iniciar servidor na porta {Porta}:\n{e.Message}");
+        }
+    }
+
+    private async Task AguardarRequests()
+    {
+        while (true)
+        {
+            Socket conexao = await this.Controlador.AcceptSocketAsync();
+            this.QtdeRequests++;
+            Task task = Task.Run(() => ProcessarRequest(conexao, this.QtdeRequests));
+        }
+    }
+
+    private void ProcessarRequest(Socket conexao, int numeroRequest)
+    {
+        Console.WriteLine($"Processando request #{numeroRequest}...\n");
+        if (conexao.Connected)
+        {
+            byte[] bytesRequisicao = new byte[1024];
+            conexao.Receive(bytesRequisicao, bytesRequisicao.Length, 0);
+            string textoRequisicao = Encoding.UTF8.GetString(bytesRequisicao)
+                .Replace((char)0, ' ').Trim();
+            if (textoRequisicao.Length > 0)
+            {
+                Console.WriteLine($"\n{textoRequisicao}\n");
+
+                string[] linhas = textoRequisicao.Split("\r\n");
+                int iPrimeiroEspaco = linhas[0].IndexOf(' ');
+                int iSegundoEspaco = linhas[0].LastIndexOf(' ');
+                string metodoHttp = linhas[0].Substring(0, iPrimeiroEspaco);
+                string recursoBuscado = linhas[0].Substring(
+                    iPrimeiroEspaco + 1, iSegundoEspaco - iPrimeiroEspaco - 1);
+                if (recursoBuscado == "/") recursoBuscado = "/index.html";
+                string textoParametros = recursoBuscado.Contains("?") ?
+                    recursoBuscado.Split("?")[1] : "";
+                SortedList<string, string> parametros = ProcessarParametros(textoParametros);
+                string dadosPost = textoRequisicao.Contains("\r\n\r\n") ?
+                    textoRequisicao.Split("\r\n\r\n")[1] : "";
+                if (!string.IsNullOrEmpty(dadosPost))
+                {
+                    dadosPost = HttpUtility.UrlDecode(dadosPost, Encoding.UTF8);
+                    var parametrosPost = ProcessarParametros(dadosPost);
+                    foreach (var pp in parametrosPost)
+                        parametros.Add(pp.Key, pp.Value);
+                }
+                recursoBuscado = recursoBuscado.Split("?")[0];
+                string versaoHttp = linhas[0].Substring(iSegundoEspaco + 1);
+                iPrimeiroEspaco = linhas[1].IndexOf(' ');
+                string nomeHost = linhas[1].Substring(iPrimeiroEspaco + 1);
+
+                byte[] bytesCabecalho = null;
+                byte[] bytesConteudo = null;
+                FileInfo fiArquivo = new FileInfo(ObterCaminhoFisicoArquivo(nomeHost, recursoBuscado));
+                if (fiArquivo.Exists)
+                {
+                    if (TiposMime.ContainsKey(fiArquivo.Extension.ToLower()))
+                    {
+                        if (fiArquivo.Extension.ToLower() == ".dhtml")
+                            bytesConteudo = GerarHTMLDinamico(fiArquivo.FullName, parametros, metodoHttp);
+                        else
+                            bytesConteudo = File.ReadAllBytes(fiArquivo.FullName);
+
+                        string tipoMime = TiposMime[fiArquivo.Extension.ToLower()];
+                        bytesCabecalho = GerarCabecalho(versaoHttp, tipoMime,
+                            "200", bytesConteudo.Length);
+                    }
+                    else
+                    {
+                        bytesConteudo = Encoding.UTF8.GetBytes(
+                            "<h1>Erro 415 - Tipo de arquivo não suportado.</h1>");
+                        bytesCabecalho = GerarCabecalho(versaoHttp, "text/html;charset=utf-8",
+                            "415", bytesConteudo.Length);
+                    }
+                }
+                else
+                {
+                    bytesConteudo = Encoding.UTF8.GetBytes(
+                        "<h1>Erro 404 - Arquivo Não Encontrado</h1>");
+                    bytesCabecalho = GerarCabecalho(versaoHttp, "text/html;charset=utf-8",
+                        "404", bytesConteudo.Length);
+                }
+                int bytesEnviados = conexao.Send(bytesCabecalho, bytesCabecalho.Length, 0);
+                bytesEnviados += conexao.Send(bytesConteudo, bytesConteudo.Length, 0);
+                conexao.Close();
+                Console.WriteLine($"\n{bytesEnviados} bytes enviados em resposta à requisição #{numeroRequest}.");
+            }
+        }
+        Console.WriteLine($"\nRequest {numeroRequest} finalizado.");
     }
 
 }
